3fc5a4b7a6841b4b567ed65e968df2fdf8de7f1f
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-07-06 07:38:35 +0800

    db: count image_label from DB (replace random placeholder) ui: explain diagram coloring with diagram remark

--- server/app/pages/stats.tsx
+++ server/app/pages/stats.tsx
@@ -17,6 +17,7 @@ import { renderError } from '../components/error.js'
 import { getAuthUser } from '../auth/user.js'
 import { evalLocale, Locale } from '../components/locale.js'
 import { proxy } from '../../../db/proxy.js'
+import { db } from '../../../db/db.js'
 
 let pageTitle = <Locale en="Stats Data" zh_hk="统计数据" zh_cn="统计数据" />
 
@@ -82,9 +83,51 @@ let page = (
   </>
 )
 
+let select_label_count = db.prepare<
+  void[],
+  { image_id: number; label_id: number; answers: string }
+>(/* sql */ `
+select
+  image.id as image_id
+, label.id as label_id
+, json_group_array(image_label.answer) as answers
+from image
+inner join label
+left join image_label
+  on image.id = image_label.image_id
+ and label.id = image_label.label_id
+group by label.id, image.id
+`)
+
 function Main(attrs: {}, context: Context) {
   let user = getAuthUser(context)
   let totalCount = <span class="stats-label-count">{proxy.label.length}</span>
+
+  // label -> {yes, no, unknown}
+  let labels: {
+    [label_id: number]: { yes: number; no: number; unknown: number }
+  } = {}
+  let rows = select_label_count.all()
+  console.log({ rows })
+  for (let row of rows) {
+    let { label_id } = row
+    let answers = JSON.parse(row.answers) as (1 | 0 | null)[]
+    labels[label_id] ||= { yes: 0, no: 0, unknown: 0 }
+    for (let answer of answers) {
+      switch (answer) {
+        case 1:
+          labels[label_id].yes++
+          break
+        case 0:
+          labels[label_id].no++
+          break
+        case null:
+          labels[label_id].unknown++
+          break
+      }
+    }
+  }
+
   return (
     <>
       <h2 class="ion-padding-horizontal">
@@ -95,12 +138,25 @@ function Main(attrs: {}, context: Context) {
           zh_cn={<>总共 {totalCount} 种标签</>}
         />
       </h2>
+      <div class="ion-margin-horizontal">
+        <span>
+          <Locale en="Diagram Remark: " zh_hk="圖解：" zh_cn="图解：" />
+        </span>
+        <div class="stats-chart" style="display: inline-flex">
+          <div class="stats-chart--bar" data-label="yes">
+            <Locale en="Yes" zh_hk="是" zh_cn="是" />
+          </div>
+          <div class="stats-chart--bar" data-label="unknown">
+            <Locale en="Unknown" zh_hk="未知" zh_cn="未知" />
+          </div>
+          <div class="stats-chart--bar" data-label="no">
+            <Locale en="No" zh_hk="否" zh_cn="否" />
+          </div>
+        </div>
+      </div>
       {mapArray(proxy.label, label => {
         let label_id = label.id!
-        count(proxy.image_label, { label_id })
-        let yes = Math.floor(Math.random() * 100)
-        let unknown = Math.floor(Math.random() * 100)
-        let no = Math.floor(Math.random() * 100)
+        let { yes, no, unknown } = labels[label_id]
         return (
           <ion-card class="stats-item">
             <ion-card-content>
@@ -116,20 +172,27 @@ function Main(attrs: {}, context: Context) {
 
 function StatsChart(attrs: { yes: number; unknown: number; no: number }) {
   let { yes, unknown, no } = attrs
+  let total = yes + unknown + no
   return (
     <div class="stats-chart">
       <div class="stats-chart--bar" data-label="yes" style={`flex: ${yes};`}>
-        {yes}
+        <span>{yes}</span>{' '}
+        <span hidden={yes === 0}>({Math.round((yes / total) * 100)}%)</span>
       </div>
       <div
         class="stats-chart--bar"
         data-label="unknown"
         style={`flex: ${unknown};`}
+        hidden={unknown === 0}
       >
-        {unknown}
+        <span>{unknown}</span>{' '}
+        <span hidden={unknown === 0}>
+          ({Math.round((unknown / total) * 100)}%)
+        </span>
       </div>
       <div class="stats-chart--bar" data-label="no" style={`flex: ${no};`}>
-        {no}
+        <span>{no}</span>{' '}
+        <span hidden={no === 0}>({Math.round((no / total) * 100)}%)</span>
       </div>
     </div>
   )
